6e2d7947d744c0d92dc39c6eb6df2853ebf2e835
iaagwl <iaagwl@gmail.com>
2017-06-29 00:14:42 +0200

    fix remove modal on gallery

--- js/navigation.js
+++ js/navigation.js
@@ -25,6 +25,7 @@ function galleryNav(){
                         createImgElements(galleryArr) :
                         emptyMessage('Your gallery is currently empty :(')
 
+  removeModal()
   contentDiv.classList.remove('loading')
   gallery(galleryElement)
 }
